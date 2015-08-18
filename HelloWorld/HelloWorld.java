f88a62b0da164ab51cf44c4a342fd8f8337e328a
GECKO1120 <gecko1120@gmail.com>
2015-08-18 09:17:35 -0700

    Hello World demonstration
    yay :D

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,15 @@
+
+/**
+ * Print Hello World.
+ * 
+ * @author Lu Peng 
+ * @version 8/18/15
+ */
+public class HelloWorld
+{
+    public static void main (String[] args)
+    {
+        System.out.println("Hello World! From Lu");
+    }
+
+}
