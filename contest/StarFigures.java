faa9a62e3368ad163fc9b3df9ae3155bf159c72b
GECKO1120 <gecko1120@gmail.com>
2015-08-25 08:57:26 -0700

    StarFigures demonstration
    :(

---
+++ contest/StarFigures.java
@@ -0,0 +1,28 @@
+public class StarFigures {
+    public static void main (String[] args){
+        Tenstar();
+        Fivestar();
+        System.out.println();
+        Tenstar();
+        Fivestar();
+        Tenstar();
+        System.out.println();
+        Threestar();
+        Tenstar();
+        Fivestar();
+    }    
+    
+    public static void Tenstar(){
+        System.out.println("*****\n*****");
+    }
+    
+    public static void Fivestar(){
+        System.out.println(" * * \n  *  \n * * ");
+    }
+    public static void Threestar(){
+        System.out.println("  *  \n  *   \n  *  ");
+    }
+}
+        
+            
+       
